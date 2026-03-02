ebdfd3c7bfb787b9b1984e49dbefda17115e25d8
smartpocketoa-code <smartpocket.oa@gmail.com>
2026-03-02 16:27:36 +0700

    Initialize Express app with LINE webhook integration

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const line = require("@line/bot-sdk");
+
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
+
+const app = express();
+
+app.post("/webhook", line.middleware(config), (req, res) => {
+  Promise.all(req.body.events.map(handleEvent))
+    .then(() => res.json({ success: true }))
+    .catch((err) => res.status(500).end());
+});
+
+const client = new line.Client(config);
+
+function handleEvent(event) {
+  if (event.type !== "message") return Promise.resolve(null);
+
+  return client.replyMessage(event.replyToken, {
+    type: "text",
+    text: "คุณพิมพ์ว่า: " + event.message.text,
+  });
+}
+
+app.listen(process.env.PORT || 3000);
